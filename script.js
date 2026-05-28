6a5a4fe04cc8654ef5deb10edfa6b586d0db0ffd
CalledJason <rendyhikmawan040@gmail.com>
2026-05-29 02:44:20 +0700

    feat: nambahin active state di bagian list navbar

--- script.js
+++ script.js
@@ -78,5 +78,12 @@ $(document).ready(function() {
         
         $('#warningMsg').removeClass('d-none');
     });
+
+    $('.navbar-nav .nav-link').on('click', function () {
+       
+        $('.navbar-nav .nav-link').removeClass('active');
+        
+        $(this).addClass('active');
+    });
 });
 
