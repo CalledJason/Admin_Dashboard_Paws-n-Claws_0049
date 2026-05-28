a552f7291fd13d36fa85ccf4803f758081af200e
CalledJason <rendyhikmawan040@gmail.com>
2026-05-28 20:35:36 +0700

    feat(js): handle pet type size dropdown dan base pricing calculation logic di radio inputs

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 $(document).ready(function {
-    
+    $('#petType').change(function() {
+        $(this).removeClass('text-muted');
+
+        $('#petSize').prop('disabled', false);
+        calculateTotal();
+    });
+
+    $('#petSize').change(function() {
+        $(this).removeClass('text-muted');
+        $('input[name=""packageRadio]').prop('disabled', false);
+        calculateTotal();
+    });
+
+    $('input[name=""packageRadio]').chang(function() {
+        calculateTotal();
+    });
+
+    function calculateTotal() {
+        let petType = $('#petType').val();
+        let petSize = $('#petSize').val();
+        let packageBasePrice = parseInt($('input[name="packageRadio"]:checked').val()) || 0;
+    }
+
+
 });
+
